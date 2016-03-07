bfb8f67da6f799e6f7307ee263c0faf0611e0425
xenin <mmg@iMAC.local>
2016-03-07 15:38:42 +1100

    composer update

--- src/RouteManager.php
+++ src/RouteManager.php
@@ -1,6 +1,17 @@
 <?php
 namespace xenin;
 
+/**
+ *
+ * A Simple REST API Route Manager for PHP.
+ * Create named routes for easy access to REST APIs.
+ * RouteManager will assemble and return complete route from pattern supplied.
+ * 
+ * @author xenin <xenin@xenin.net>
+ * @link https://github.com/xenin/RouteManager
+ * 
+ */
+
 class RouteManager {
 	
 	protected $opt = array(
@@ -18,18 +29,37 @@ class RouteManager {
         $this->opt = null;
     }
 
+    /**
+     * Set base url
+     *
+     * @param string $url       API base url
+     */
     public function base($url = null) {
         if($url !== null) {
             $this->opt["base"] = $url;
         }
     }
     
+    /**
+     * Add route
+     *
+     * @param string $name      Name of route
+     * @param string $route     Route pattern eg. /user/{userID}/account
+     */
     public function add($name = null, $route = null) {
     	if($name !== null && $route !== null) {
     		$this->opt["routes"][$name] = $route;
     	}
     }
     
+    /**
+     * Get route
+     *
+     * @param string $name      Name of route
+     * @param array $values     Route pattern variables
+     *
+     * @return string|null      Complete url from pattern
+     */
     public function get($name = null, $values = array()) {
         if($this->opt["base"] === null) {
             return null;
@@ -57,13 +87,20 @@ class RouteManager {
                 if (strncmp($route, $this->opt["base"], strlen($this->opt["base"])) !== 0) {
                     $route = rtrim($this->opt["base"], '/') . '/' . ltrim($route, '/');
                 }
-    			return $route;
+    			return urlencode($route);
     		} else {
     			return null;
     		}
     	}
     }
     
+    /**
+     * Find pattern keys
+     *
+     * @param string $route     Route pattern
+     *
+     * @return array            Match array
+     */
     protected function getRouteKeys($route) {
     	preg_match_all("/{[a-zA-Z_]+}/", $route, $ret);
     	return $ret;
